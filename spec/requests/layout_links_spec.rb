b513fa4d14a1fb422e06aa96ec296eb7ac49b136
Aron Ritchie <enderfour@gmail.com>
2010-11-30 18:57:34 -0800

    incremental

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -7,24 +7,38 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Home")
   end  
 
-  it "should have a Home page at '/contact'" do
+  it "should have a contact page at '/contact'" do
     get '/contact'
     response.should have_selector('title', :content => "Contact")
   end  
 
-  it "should have a Home page at '/about'" do
+  it "should have an about page at '/about'" do
     get '/about'
     response.should have_selector('title', :content => "About")
   end  
 
-  it "should have a Home page at '/help'" do
+  it "should have a help page at '/help'" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end  
 
-  it "should have a Home page at '/signup'" do
+  it "should have a sign up page at '/signup'" do
     get '/signup'
     response.should have_selector('title', :content => "Sign up")
-  end  
+  end
+
+  #it "should have the right links on the layout" do
+  # visit root_path
+  #  click_link "About"
+  #  response.should have_selector('title', :content => "About")
+  #  click_link "Help"
+  #  response.should have_selector('title', :content => "Help")
+  #  click_link "Contact"
+  #  response.should have_selector('title', :content => "Contact")
+  #  click_link "Home"
+  #  response.should have_selector('title', :content => "Home")
+  #  click_link "Sign up now!"
+  #  response.should have_selector('title', :content => "Sign up")
+  #end
 
 end
