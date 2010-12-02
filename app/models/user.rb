21e8d40509e1c1fe820e107730e1c38bed79375c
Aron Ritchie <enderfour@gmail.com>
2010-12-01 16:18:21 -0800

    finished user model with passwords and show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,50 +10,117 @@
 #  updated_at :datetime
 #
 
+require 'digest'
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  include ActiveModel::Validations
 
-  # special - special characters that may appear in the local_part of an
-  # email address
-  special = Regexp.escape('!#$%&\'*+-/=?^`{|}~')
+  class EmailValidator < ActiveModel::EachValidator
+    def validate_each( record, attribute, value )
 
-  # word - matches a series of letters, digits and certain special characters
-  # (! # $ % & ' * + - / = ? ^ _ ` { | } ~)
-  word = "[\\w#{special}]+"
-  
-  # local_part - matches the part of an email address before the at sign (@).
-  # consists of a series of letters, digits and certain special characters,
-  # including one or more dots. However, dots may not appear consecutively
-  # or at the start or end of the word
-  local_part = "(#{word})(\\.(#{word}))*"
-  
-  # label - matches a string of alphanumeric characters and dashes (-). However,
-  # dashes may not appear at the start or end of a label
-  label = "[a-z0-9]+(\\-[a-z0-9]+)*"
+      # special characters that may appear in the local_part of an email address
+      special_chars = Regexp.escape('!#$%&\'*+-/=?^_`{|}~')
 
-  # tld - top level domain - matches top level domains, for simplicities sake
-  # it will match any string of letters of length 2-4 inclusive, or the museum
-  # domain as it is currently the only top level domain with more than 4 letters
-  tld = "[a-z]{2,4}|museum"
+      # all characters that are valid  in the local part of an email address,
+      # includes all alphanumeric characters and certain special characters
+      # (! # $ % & ' * + - / = ? ^ _ ` { | } ~), except for dot (.)
+      all_chars = "[a-z0-9#{special_chars}]"
+      
+      # the local part matches the part of an email address before the at sign (@).
+      # consists of a series of letters, digits and certain special characters,
+      # including one or more dots. However, dots may not appear consecutively
+      # or at the start or end of the word
+      local_part = "(#{all_chars})+(\\.(#{all_chars})+)*"
+      
+      # a label matches the parts of the domain of an email address that are seperated
+      # by dots (.). consist of a string of alphanumeric characters and dashes (-).
+      # However, dashes may not appear at the start or end of a label
+      label = "[a-z0-9]+(\\-[a-z0-9]+)*"
 
-  # domain - matches the part of an email address after the at sign (@).
-  # valid domains are of the form label(.label)*.tld
-  domain = "(#{label})(\\.(#{label}))*\\.(#{tld})"
+      # the top level domain matches the charcters after the last dot (.) in an email.
+      # for simplicities sake it will match any string of letters of length 2-4 inclusive,
+      # or the museum domain as it is currently the only top level domain with more than
+      # 4 letters
+      top_level_domain = "[a-z]{2,4}|museum"
 
-  # email_regex - matches strings of the form local_part@domain
-  #email_regex = /\A(#{local_part})@(#{domain})\z/i
-  email_regex = Regexp.new( "\\A(#{local_part})@(#{domain})\\z", Regexp::IGNORECASE )
+      # the domain matches the part of an email address after the at sign (@).
+      # valid domains are of the form label(.label)*.tld
+      domain = "(#{label})(\\.(#{label}))*\\.(#{top_level_domain})"
 
-  validates :name,  :presence   => true,
-                    :length     => { :maximum => 64 }
-  validates :email, :presence   => true,
-                    :format     => { :with => email_regex },
-                    :length     => { :maximum => 254 },
-                    :uniqueness => { :case_sensitive => false }
-  #validates validate_local_part
-  
-  #def validate_local_part
-  #  errors.add_to_base "The local part of an email address (the part before @) may not be longer than 64 characters" if :email.to_s.split('@')[0].length > 64 unless :email.nil?
-  #end
+      # email_regex - matches strings of the form local_part@domain
+      #email_regex = /\A(#{local_part})@(#{domain})\z/i
+      email_regexp = Regexp.new( "\\A(#{local_part})@(#{domain})\\z", Regexp::IGNORECASE )
+
+      #record.errors[ attribute ] << ( options[ :message ] || "is not an email" ) unless
+      #  value =~ email_regexp
+
+      if !( value =~ email_regexp )
+        return record.errors[ attribute ] << ( options[ :message ] || "is not an email" )  
+      end
+
+      # the local part of an email address is limited to 64 characters or less
+      local_part_length = 64
+      local_part_length_regexp = /\A[^@]{0,#{local_part_length}}@/i
+      
+      if !( value =~ local_part_length_regexp )
+        return record.errors[ attribute ] << ( options[ :message ] || "is not an email" )  
+      end                
+
+      # the entire email address is limited to 254 characters or less
+      email_address_length = 254
+      email_address_length_regexp = /\A.{0,#{email_address_length}}\z/i
+      
+      if !( value =~ email_address_length_regexp )
+        return record.errors[ attribute ] << ( options[ :message ] || "is not an email" )  
+      end
+                    
+    end
+  end
+
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
+
+
+  validates :name,     :presence     => true,
+                       :length       => { :maximum => 64 }
+  validates :email,    :presence     => true,
+                       :email        => true,
+                       #:format       => { :with => email_regex },
+                       #:length       => { :maximum => 254 },
+                       :uniqueness   => { :case_sensitive => false }
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+  # returns true if the user's password matches the submitted password
+  def has_password?( submitted_password )
+    encrypted_password == encrypt( submitted_password )
+  end
+
+  def self.authenticate( email, submitted_password )
+    user = find_by_email( email )
+    #return nil if user.nil?
+    return user if user.has_password?( submitted_password ) unless user.nil?
+  end
+
+  private
+
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt( password )
+    end
+
+    def encrypt( string )
+      secure_hash( "#{salt}--#{string}" )
+    end
+
+    def make_salt
+      secure_hash( "#{Time.now.utc}--#{password}" )
+    end
+
+    def secure_hash( string )
+      Digest::SHA2.hexdigest( string )
+    end
   
 end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name "Aron Ritchie"
+  user.email "aron.ritchie@foo.bar.com"
+  user.password "bazbix"
+  user.password_confirmation "bazbix"
+end
