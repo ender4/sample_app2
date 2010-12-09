19526b457d515531717ebe661a7290dceca71976
Aron Ritchie <enderfour@gmail.com>
2010-12-08 16:43:13 -0800

    fixed small error in microposts model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -19,5 +19,5 @@ class Micropost < ActiveRecord::Base
   
   validates :user_id, :presence => true
   validates :content, :presence => true,
-                      :length => { :maximum => 140 },
+                      :length => { :maximum => 140 }
 end
